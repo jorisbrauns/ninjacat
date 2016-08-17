87e98a71086e1e4017cc7ab5345d85547c849a99
Joris Brauns <mr.joris@gmail.com>
2016-08-17 17:49:51 +0200

    Update ninjacat-prototype.js

--- js/ninjacat-prototype.js
+++ js/ninjacat-prototype.js
@@ -1,59 +1,95 @@
-$(function() {
+$(function () {
 
-        $("body").append('<img src="img/win10_skype_320x320.0.gif" class="ninjacat">')
-        $(".ninjacat").hide();
-    
-        var enabled = false; var i=0;
-        $(window).keypress(function( event ) {
-             event.preventDefault();
-            if ( event.which === 32  ) {
-                i++;
-                if(i===4){
+    $("body").append('<img src="images/win10_skype_320x320.0.gif" class="ninjacat">');
+    $(".ninjacat").click(function() {
+        window.location = 'https://insider.windows.com/';
+    }).hide();
+
+    var enabled = false; var i = 0;
+    var lastInput = "";
+    var mutex = false;
+    var codes = [];
+    var correctCodes = [78,73,78, 74, 65, 67, 65, 84];
+    $(document).bind('keydown', function (evt) {
+
+        if (!enabled) {
+            codes.push(evt.keyCode);
+
+
+            for (var j = 0; j < correctCodes.length; j++) {
+                if (codes.length > j) {
+                    if (correctCodes[j] !== codes[j]) {
+                        codes = [];
+                    }
+                }
+
+                if (codes.join("") === '7873787465676584') {
                     enabled = true;
                 }
             }
-            if(enabled){
+
+            if (enabled) {
                 $('.ninjacat').show();
+            } else {
+                return;
             }
+        }
 
-        });
+        if (evt.keyCode !== 39 &&
+            evt.keyCode !== 65 &&
+            evt.keyCode !== 81 &&
+            evt.keyCode !== 87 &&
+            evt.keyCode !== 68 &&
+            evt.keyCode !== 90 ) return;
 
-        var mutex = false;
-        $(document).bind('keydown',function(evt) {
-            if(evt.keyCode !== 39 && evt.keyCode !==37 && evt.keyCode !==38) return;
-                
-            if(!mutex){
-                mutex = true;
-                move();
-            }
-                
-            function move(){
-                if(enabled){
-                    switch(evt.keyCode) {
-                        case 39: // right
-                        $( ".ninjacat" ).removeClass('ninjacat-left').animate({
-                            left: (parseInt($( ".ninjacat" ).css('left').replace('px','')) +40 ) + 'px'
-                        }, 200 , "linear", function() {
+        if (!mutex) {
+            mutex = true;
+            move();
+        }
+
+        function move() {
+            console.log(evt.keyCode);
+
+            if (enabled) {
+                switch (evt.keyCode) {
+                    case 68: // right
+                        $(".ninjacat").removeClass('ninjacat-left').animate({
+                            left: (parseInt($(".ninjacat").css('left').replace('px', '')) + 40) + 'px'
+                        }, 200, "linear", function () {
                             mutex = false;
                         });
                         break;
-                        case 37: // left
-                        $( ".ninjacat" ).addClass('ninjacat-left').animate({
-                            left: (parseInt($( ".ninjacat" ).css('left').replace('px','')) - 40 ) + 'px'
-                        }, 200 , "linear", function() {
+                    case 65: // left
+                    case 81: // left
+                        $(".ninjacat").addClass('ninjacat-left').animate({
+                            left: (parseInt($(".ninjacat").css('left').replace('px', '')) - 40) + 'px'
+                        }, 200, "linear", function () {
                             mutex = false;
                         });
                         break;
-                        case 38: // jump
-                        $('.ninjacat').animate({ bottom: 305, opacity: '1' }, 200,"linear",function(){
-                            $('.ninjacat').animate({ bottom: -79, opacity: '1' }, 1000,"easeOutBounce",function(){
-                                mutex = false;
-                            })
-                        })
+                    case 87: // jump
+                    case 90: // jump
+                        $('.ninjacat')
+                            .animate({ bottom: -$(window).scrollTop() + 160, opacity: '1' },
+                                200,
+                                "linear",
+                                function() {
+                                    $('.ninjacat')
+                                        .animate({ bottom: -$(window).scrollTop(), opacity: '1' },
+                                            1000,
+                                            "easeOutBounce",
+                                            function() {
+                                                mutex = false;
+                                            });
+                                });
                         break;
-                    }
                 }
             }
-        });
-
+        }
     });
+
+    $(window).scroll(function () {
+        $(".ninjacat").css('bottom', -$(window).scrollTop() + 'px');
+    });
+
+});
